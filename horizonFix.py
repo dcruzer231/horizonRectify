f12d4fc9db299102ec44f271fe79cddfe34374f0
Daniel Cruz <dcruzer231@gmail.com>
2023-10-09 11:51:34 -0600

    matches horizon level to a golden standard

--- horizonFix.py
+++ horizonFix.py
@@ -7,8 +7,15 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import os
 
+from PIL import Image, ExifTags
+
+from datetime import datetime
+
 #last line of the image before timestamp
 imgend = 2299
+goldB = 589.2652532259266
+goldA = 0.006334331804510105
+
 
 
 def detect_horizon_line(image_grayscaled):
@@ -52,6 +59,36 @@ def detect_horizon_line(image_grayscaled):
     return x,y,image_closed
     return horizon_x1, horizon_x2, horizon_y1, horizon_y2
 
+def warp(img,matrix):
+    return cv2.warpAffine(img, matrix, (img.shape[1], img.shape[0]))
+
+def getRotationMatrix(img,angle):
+    width,height = img.shape[:2]
+    center = (width//2, height//2)
+    return cv2.getRotationMatrix2D(center=center, angle=angle, scale=1)
+
+def getVerticalShiftMatrix(img,a,b,goldA,goldB):
+    width,height = img.shape[:2]
+    center = (width//2, height//2)
+    
+    horizonHeight = a*center[1] + b
+    goldHorizonHeight = goldA*center[1] + goldB
+    
+    diff = goldHorizonHeight - horizonHeight
+    return np.array([[1,0,0],
+                     [0,1,diff]])
+
+    
+
+#datetime.isoformat()
+def getDateTime(imdir):
+    pimg = Image.open(str(imdir))
+    img_exif = pimg.getexif()
+    #this is the code for datetime
+    strdatetime = img_exif[306]
+    timestamp = datetime.strptime(strdatetime,"%Y:%m:%d %H:%M:%S")
+    return timestamp
+
 
 if __name__ == '__main__':
     rotation_save_dir = Path(r"C:\Users\Daniel\Documents\sel\horizon_rotation_images\goldenStandardRotated")
@@ -60,23 +97,31 @@ if __name__ == '__main__':
 
     data_dir = Path(r"C:\Users\Daniel\Documents\sel\Example_Images_For_Rectification")
     data_dir = Path(r"C:\Users\Daniel\Documents\sel\horizon_rotation_images\goldenStandards")
+    data_dir = Path(r"D:\ITEX-AON_Phenocam_Images\WingScapes_PhenoCam_2011-2015\Utqiagvik_MISP_PhenoCam")
 
-    input_img_paths = sorted(
-    [
-        os.path.join(data_dir, fname)
-        for fname in os.listdir(data_dir)
-        if fname.endswith(".JPG") or fname.endswith(".jpg")
-    ]
-    )
+    # input_img_paths = sorted(
+    # [
+    #     os.path.join(data_dir, fname)
+    #     for fname in os.listdir(data_dir)
+    #     if fname.endswith(".JPG") or fname.endswith(".jpg")
+    # ]
+    # )
+    
+    files = list(data_dir.glob("**/*"))
+    input_img_paths = [x for x in files if x.is_file()]
+
+
     
     print(input_img_paths)
     
     i = 1
-    for imdir in input_img_paths:
+    for imdir in input_img_paths[50:51]:
         # img = cv2.imread(r"C:\Users\Daniel\Documents\sel\Example_Images_For_Rectification\WSBC0029.JPG",cv2.IMREAD_GRAYSCALE)
         #img = cv2.imread(imdir,cv2.IMREAD_GRAYSCALE)
         name = Path(imdir).stem
-        img = cv2.imread(imdir)
+        img = cv2.imread(str(imdir))
+        timestamp = getDateTime(str(imdir))
+        
         grnimg = img[:,:,0]
         width,height = grnimg.shape[:]
 
@@ -107,15 +152,19 @@ if __name__ == '__main__':
         grnname = name + "_" + "green" + ".jpg"
         #cv2.imwrite(str(green_save_dir/grnname),grnimg)
         
+        rot_matrix = getRotationMatrix(img,angle)
+        vert_matrix = getVerticalShiftMatrix(img,a,b,goldA,goldB)
+        matrix = vert_matrix * rot_matrix  
+
+
+        rot_img = warp(img, rot_matrix)
+        rot_img = warp(rot_img, vert_matrix)
         
-        center = (width//2, height//2)
-        matrix = cv2.getRotationMatrix2D(center=center, angle=angle, scale=1)
-        rot_img = cv2.warpAffine(img, matrix, (img.shape[1], img.shape[0]))
         plt.figure(i+1)
         i+=2
-        # plt.imshow(rot_img)
+        plt.imshow(rot_img)
         
-        rotname = name + "_" + str(round(angle,4)) + ".jpg"
+        rotname = name + "_" + str(round(angle,4)) +"_" + timestamp.isoformat() + "_" + ".jpg"
         #cv2.imwrite(str(rotation_save_dir/rotname),rot_img)
         
         # plt.imsave("")
