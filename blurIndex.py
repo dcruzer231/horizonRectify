43efc401892b2adaf6bc9bdfff66da90c8df444a
Daniel Cruz <dcruzer231@gmail.com>
2024-02-07 18:13:30 -0700

    remove duplicate code

--- blurIndex.py
+++ blurIndex.py
@@ -1,108 +1,8 @@
 
 import cv2
-
 import numpy as np
-import matplotlib.pyplot as plt
 from pathlib import Path
-import os
 
-from PIL import Image, ExifTags
-
-from datetime import datetime
-from tqdm import tqdm
-
-#last line of the image before timestamp
-imgend = 2299
-#goldB = 589.2652532259266
-goldB = 582.3542738867756
-
-#goldA = 0.006334331804510105
-goldA = -0.047456609746488194
-
-
-#inspired by method developed by Sean Sall origin: https://github.com/sallamander/horizon-detection/blob/master/utils.py
-def detect_horizon_line(image_grayscaled):
-    """Detect the horizon's starting and ending points in the given image
-
-    The horizon line is detected by applying Otsu's threshold method to
-    separate the sky from the remainder of the image.
-
-    :param image_grayscaled: grayscaled image to detect the horizon on, of
-     shape (height, width)
-    :type image_grayscale: np.ndarray of dtype uint8
-    :return: the (x1, x2, y1, y2) coordinates for the starting and ending
-     points of the detected horizon line
-    :rtype: tuple(int)
-    """
-
-    msg = ('`image_grayscaled` should be a grayscale, 2-dimensional image '
-           'of shape (height, width).')
-    assert image_grayscaled.ndim == 2, msg
-    image_blurred = cv2.GaussianBlur(image_grayscaled, ksize=(3, 3), sigmaX=0)
-
-    _, image_thresholded = cv2.threshold(
-        image_blurred, thresh=0, maxval=1,
-        type=cv2.THRESH_BINARY+cv2.THRESH_OTSU
-    )
-    image_thresholded = image_thresholded - 1
-    image_closed = cv2.morphologyEx(image_thresholded, cv2.MORPH_CLOSE,
-                                    kernel=np.ones((9, 9), np.uint8))
-
-    horizon_x1 = 0
-    horizon_x2 = image_grayscaled.shape[1] - 1
-    horizon_y1 = max(np.where(image_closed[:, horizon_x1] == 0)[0])
-    horizon_y2 = max(np.where(image_closed[:, horizon_x2] == 0)[0])
-
-    #get x coordinates of the threshold in 10 pixel steps
-    x = np.arange(horizon_x1,horizon_x2,10)
-    y = []
-    #Find y coordinates that land on the edge of the threshold
-    for i in x:
-        y.append(min(np.where(image_closed[:, i] == 255)[0]))
-    return x,y,image_closed
-
-#warps image given a matrix
-def warp(img,matrix):
-    return cv2.warpAffine(img, matrix, (img.shape[1], img.shape[0]))
-
-#finds rotation matrix based off of angle in degrees
-def getRotationMatrix(img,angle):
-    width,height = img.shape[:2]
-    center = (width//2, height//2)
-    return cv2.getRotationMatrix2D(center=center, angle=angle, scale=1)
-
-#finds matrix for vertical shifting to align to golden standard. Uses the line of best fit from the golden standard
-def getVerticalShiftMatrix(img,a,b,goldA,goldB):
-    width,height = img.shape[:2]
-    center = (width//2, height//2)
-    
-    horizonHeight = a*center[1] + b
-    goldHorizonHeight = goldA*center[1] + goldB
-    
-    diff = goldHorizonHeight - horizonHeight
-    return np.array([[1,0,0],
-                     [0,1,diff]])
-
-    
-#retrieves data time from exif
-def getDateTime(imdir):
-    pimg = Image.open(str(imdir))
-    img_exif = pimg.getexif()
-    #this is the code for datetime
-    strdatetime = img_exif[306]
-    timestamp = datetime.strptime(strdatetime,"%Y:%m:%d %H:%M:%S")
-    return timestamp
-
-#returns directory structure with parent directories removed
-def getfilestructure(path):
-    return str(path.parent).replace(str(data_dir),"")[1:] #indexing to remove starting /
-
-def writetocsv(filename,row):
-    import csv
-    with open(filename, '+a') as f_object:
-        writer_object = csv.writer(f_object)
-        writer_object.writerow(row)
-        f_object.close()
 
 def variance_of_laplacian(image):
     # compute the Laplacian of the image and then return the focus
@@ -117,9 +17,6 @@ def isBlurry(img,threshhold):
     else:
         return True,fm
 
-
-
-    
 
 if __name__ == '__main__':
     rotation_save_dir = Path(r"C:\Users\Daniel\Documents\sel\horizon_rotation_images\goldenStandardRotated_nostamp")
@@ -147,6 +44,7 @@ if __name__ == '__main__':
     i = 1
     blurs = []
     for imdir in tqdm(input_img_paths):
+        from tqdm import tqdm
         name = Path(imdir).stem
         img = cv2.imread(str(imdir))
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
